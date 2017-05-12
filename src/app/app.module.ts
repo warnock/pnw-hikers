a8bb82d718dd994361910bd01190f98a7301afdb
Megan Warnock <megandwarnock@gmail.com>
2017-05-12 15:14:04 -0700

    integrate firebase database and create sample json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from "angular2-materialize";
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,14 @@ import { AboutComponent } from './about/about.component';
 import { HikeListComponent } from './hike-list/hike-list.component';
 import { NewHikeComponent } from './new-hike/new-hike.component';
 import { routing } from './app.routing';
+import { masterFirebaseConfig } from './api-keys';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
